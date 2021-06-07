a43a1f1897673f4cde279a9485bdf869b9bbb192
Prashanth <prashanth.royal2046@gmail.com>
2021-06-07 13:27:39 +0530

    Added: Valid First Name

--- userRegistration.java
+++ userRegistration.java
@@ -1,1 +1,16 @@
-Welcome to User Registration Problem
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class userRegistration {
+    public static boolean validFirstName(String name) {
+        String s ="^([A-Z][a-z]{3,30})$";
+        Pattern p = Pattern.compile(s);
+        Matcher m = p.matcher(name);
+        return m.matches();
+    }
+    public static void main(String[] args) {
+        String firstName="Prashanth";
+        System.out.println(validFirstName(firstName));
+
+    }
+}
