f3447ae15987c995eb8c2ea05afbcc3d01761daf
Prashanth <prashanth.royal2046@gmail.com>
2021-06-07 14:06:20 +0530

    Added: Email Verification using parameterised

--- userRegistration.java
+++ userRegistration.java
@@ -1,41 +1,45 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Arrays;
+import java.util.Collection;
 
-public class userRegistration {
-    public static boolean validFirstName(String name) {
-        String s ="^([A-Z][a-z]{3,30})$";
-        Pattern p = Pattern.compile(s);
-        Matcher m = p.matcher(name);
-        return m.matches();
-		}
+	public class emailVerification {
 
-    public static boolean validEmail(String text) {
-        String s ="(^[a-zA-Z 1-9]+ *([.+-_]?[a-zA-Z 1-9]+)@([a-zA-Z 1-9][-]?)+[.][a-zA-Z ]{2,4}(.[a-zA-Z]{2})*$)";
-        Pattern p=Pattern.compile(s1);
-        Matcher m= p.matcher(text);
-        return m.matches();
-    }
+	private String emailCheck;
+	private boolean expectedResult;
 
-    public static boolean validMobileNum(String number) {
-        String s ="^([0-9]{2}[: :][0-9]{10})$";
-        Pattern P=Pattern.compile(s);
-        Matcher M= P.matcher(number);
-        return M.matches();
-    }
+	public emailVerification(String emailCheck, boolean expectedResult)
+	{
+		this.emailCheck=emailCheck;
+		this.expectedResult=expectedResult;
+	}
 
-    public static void main(String[] args) {
-        String firstName="Prashanth";
-        System.out.println(validFirstName(firstName));
+	public static Collection getInput()
+	{
+		return Arrays.asList(new Object[][] {
 
-        String lastName="Royal";
-        System.out.println(validMatchesFirstName(lastName));
+			 { "abc@yahoo.com",true},
+			 { "abc-100@yahoo.com",true},
+			 {"abc.100@yahoo.com",true},
+			 {"abc111@abc.com",true},
+			 {"abc-100@abc.net",true},
+			 {"abc.100@abc.com.au",true},
+			 {"abc@1.com",true},
+			 {"abc@gmail.com.com",true},
+			 {"abc+100@gmail.com",true},
+			 {"abc",false},
+			 {"abc@.com.my",false},
+			 {"abc123@gmail.a",false},
+			 {"abc123@.com",false},
+			 {"abc123@.com.com",false},
+			 {"abc()*@gmail.com",false},
+			 {".abc@abc.com",false},
+			 {"abc()*@gmail.com",false},
+			 {"abc@%*.com",false},
+			 {"abc.@gmail.com",false},
+			 {"abc@gmail.com.1a",false},
+			 {"abc@gmail.com.aa.au",false},
+			 {"abc....2002@gmail.com",false}
 
-		  String email="prashanth.royal@bl.co.in";
-        System.out.println(validEmail(email));    
-
-		  String mobileNum="91 9876543210";
-        System.out.println(validMobileNum(mobileNum));
-
+		});
 	}
 }
 
