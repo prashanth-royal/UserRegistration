e07189bfb621f8b0594098daf82ab424163cbae0
Prashanth <prashanth.royal2046@gmail.com>
2021-06-07 13:37:31 +0530

    Added: Valid Email

--- userRegistration.java
+++ userRegistration.java
@@ -7,6 +7,13 @@ public class userRegistration {
         Pattern p = Pattern.compile(s);
         Matcher m = p.matcher(name);
         return m.matches();
+    public static boolean validEmail(String text)
+    {
+        String s1 ="(^[a-zA-Z 1-9]+ *([.+-_]?[a-zA-Z 1-9]+)@([a-zA-Z 1-9][-]?)+[.][a-zA-Z ]{2,4}(.[a-zA-Z]{2})*$)";
+        Pattern p=Pattern.compile(s1);
+        Matcher m= p.matcher(text);
+        return m.matches();
+
     }
     public static void main(String[] args) {
         String firstName="Prashanth";
@@ -14,5 +21,8 @@ public class userRegistration {
 
         String lastName="Royal";
         System.out.println(validMatchesFirstName(lastName));
-    }
+
+		  String email="prashanth.royal@bl.co.in";
+        System.out.println(validEmail(email));    
+}
 }
